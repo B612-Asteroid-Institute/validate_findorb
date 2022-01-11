88544e7d252700b6a7b3ebf32bfca70908f3de35
berres2002 <berres2002@gmail.com>
2022-01-11 15:13:07 -0800

    Added demo and cleaned up code

--- validate_findorb/test_findorb.py
+++ validate_findorb/test_findorb.py
@@ -11,7 +11,6 @@ backend=FINDORB()
 def sear(di):
     li=os.listdir(di)
     for i in li:
-        #print(i)
         if 'run' in i:
             return i
     return 'no_dir'
@@ -26,12 +25,10 @@ def ch(di):
 def bashgen(out_dir):
     od = os.path.join(out_dir,'masterRunTEST.sh')
     val=Time.now().utc.value.isoformat(timespec='seconds')
-    #print(out_dir)
     copy(os.path.dirname(os.path.abspath(__file__))+'/eph2ades.py',out_dir+'/eph2ades.py')
     with open(od,'wt') as f:
         f.write('#!/bin/sh'+"\n")
         ed1=os.path.join(out_dir,'ephemeris/500/')
-        #print(ed1)
         f.write('cd ./ephemeris/500/'+'\n')
         f.write('bash '+f"'{sear(ed1)}'"+'\n'+'cd ..'+'\n'+'cd ..'+'\n')
         psvp=f'gen_{val}_observations.psv' # will integrate this into eph2ades.py
@@ -47,13 +44,9 @@ def bashgen(out_dir):
         f.write('bash '+f"'{sear(ed)}'")
 
 def testFO(orbit, observatory_code, t0, dts, astrometric_error=None, backend=FINDORB(),out=None):
-    #np.random.seed(3)
     if out is None:
         outd=None
-       # youd = None
     else:
-        #youd = str(uuid.uuid4())
-        #outd=os.path.join(out,youd)
         whq1=orbit.ids[0].split(' ')
         nam='_'.join(whq1)
         outd=os.path.join(out,f"{nam}/{dts.max()}days_{10.:0.0f}mas_{Time.now().utc.value.isoformat(timespec='seconds')}")
@@ -62,7 +55,6 @@ def testFO(orbit, observatory_code, t0, dts, astrometric_error=None, backend=FIN
     observation_times = t0 + dts
     obs = {observatory_code:observation_times}
     ephemeris,ret1= backend._generateEphemeris(orbits=orbit,observers =obs, out_dir=outd)
-    #writeCall(ret1,'ephemeris',outd)
     if astrometric_error is None:
         astrometric_error = 0
 
@@ -75,14 +67,13 @@ def testFO(orbit, observatory_code, t0, dts, astrometric_error=None, backend=FIN
     # Add a simple astrometric errors to both RA and Dec
     ephemeris.loc[:, "RA_deg"] += np.random.normal(loc=0, scale=astrometric_error*MAS_TO_DEG, size=len(ephemeris)) / np.cos(np.radians(ephemeris['Dec_deg'].values))
     ephemeris.loc[:, "Dec_deg"] += np.random.normal(loc=0, scale=astrometric_error*MAS_TO_DEG, size=len(ephemeris))
-    #observationsToADES(ephemeris,'./',orbit.ids[0].split()[0]+'error')
-    # Run OD with the desired backend
+
     od_orbit_df, residuals,ret2 = backend._orbitDetermination(ephemeris, out_dir=outd)
-    #writeCall(ret2,'orbit_determination',outd)
+ 
     od_orbit = Orbits.from_df(od_orbit_df)
     
     prop_orbit,ret3 = backend._propagateOrbits(orbit, observation_times[-1:], out_dir=outd)
-    #writeCall(ret3,'propagation',outd)
+
     delta_state = od_orbit_df[["x", "y", "z", "vx", "vy", "vz"]].values - prop_orbit[["x", "y", "z", "vx", "vy", "vz"]].values
     
     delta_r = np.linalg.norm(delta_state[:,0:3])
@@ -119,4 +110,4 @@ def testFO(orbit, observatory_code, t0, dts, astrometric_error=None, backend=FIN
     if out is not None:
         bashgen(outd)
     
-    return result#,ret1,ret2,ret3#,residuals
+    return result
